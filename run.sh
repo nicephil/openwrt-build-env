7f7021fb6b8bb4f5c3fbd69d5b343d1408524e59
nicephil <nicephil@gmail.com>
2020-03-05 22:29:33 +0800

    fix

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@ docker container stop openwrt-build-env
 docker container rm openwrt-build-env
 docker run \
     -itd \
+    --restart=always \
     --name openwrt-build-env \
     -h COMPILER \
     -p 10022:22 \
-    -v /home/llwang/repos:/home/llwang/repos \
+    -v /home/llwang:/home/llwang \
     nicephil/openwrt-build-env
